679f25a46c4665bf5992a10f23f13fee023fb216
Victor Padi <elnavegadordeweb@gmail.com>
2022-08-20 11:54:00 +0200

    Add file for image read/write operations

---
+++ source/steganography/imageIO.go
@@ -0,0 +1,36 @@
+package steganography
+
+import (
+	"image"
+	_ "image/gif"
+	_ "image/jpeg"
+	"image/png"
+	"log"
+	"os"
+)
+
+func ReadImage(pathToFile string, format string) image.Image {
+	reader, err := os.Open(pathToFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer reader.Close()
+	m, _, err := image.Decode(reader)
+
+	return m
+}
+
+func WriteImage(pathToFile string, imageData image.Image) {
+	f, err := os.Create("outimage.png")
+	if err != nil {
+		// Handle error
+	}
+	defer f.Close()
+
+	// Encode to `PNG` with `DefaultCompression` level
+	// then save to file
+	err = png.Encode(f, imageData)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
