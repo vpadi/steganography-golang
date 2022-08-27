ed45fb283e0f7579f656b810dc0768ccad2007a0
Victor Padi <elnavegadordeweb@gmail.com>
2022-08-27 12:41:57 +0200

    Add basic logic to distinguish w/r mode

--- source/steganographer.go
+++ source/steganographer.go
@@ -1,7 +1,34 @@
 package main
 
-import "./steganography"
+import (
+	"fmt"
+	"strings"
+)
+
+func readMessage() {
+
+}
+
+func writeMessage() {
+
+}
 
 func main() {
-	steganography.ReadImage()
+	var programMode string
+	fmt.Print("Do you want to write or read a image message? ")
+	fmt.Scanf("%s", &programMode)
+
+	programMode = strings.ToLower(programMode)
+	for programMode != "write" && programMode != "read" {
+		fmt.Print("Please, enter write or read. ")
+		fmt.Scanf("%s", &programMode)
+
+		programMode = strings.ToLower(programMode)
+	}
+
+	if programMode == "read" {
+		readMessage()
+	} else {
+		writeMessage()
+	}
 }
